5440836beca04a3fee9a1c9ba5e9a0f21f597a16
Bence Rigler <rigbence@gmail.com>
2019-10-26 17:40:15 +0200

    connecting to the database with mongoose

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const express = require('express')
 const dotenv = require('dotenv')
-const logger = require('./middleware/logger')
-//route files
-
-const bootcamps = require('./routes/bootcamps')
-
+const morgan = require('morgan')
+const connectDB = require('./config/db')
 // load env vars
 
 dotenv.config({path:'./config/config.env'})
 
+//connect to the database
+connectDB();
+
+
+//route files
+
+const bootcamps = require('./routes/bootcamps')
+
+
 const app = express();
 
 
-app.use(logger)
+/* app.use(logger)
+ *///dev logging middleware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+
 
 //Mount routers
 
@@ -21,4 +33,10 @@ app.use('/api/v1/bootcamps',bootcamps)
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`listening${process.env.NODE_ENV}`))
+const server = app.listen(PORT, console.log(`listening${process.env.NODE_ENV}`))
+
+process.on('unhandledRejection',(err,promise)=>{
+    console.log(`Error: ${err.message}`)
+    //close server exit process
+    server.close(()=> process.exit(1))
+})
